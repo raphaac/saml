77d7965cc676c6a8f64aa8ac0f9cdd4ed8d0c97c
Raphael A Correa <rapha.ac@gmail.com>
2023-08-07 22:04:35 -0300

    feature: test api with service

--- TRIESSO/Infra/AclService.cs
+++ TRIESSO/Infra/AclService.cs
@@ -1,6 +1,8 @@
 ﻿using Flurl.Http.Configuration;
 using Microsoft.AspNetCore.WebUtilities;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
@@ -15,20 +17,19 @@ namespace Infra
         {
             //var urlBase = "https://win-nj6v2gg0m3q.trie.domain/adfs/ls";
             var urlBase = "https://jsonplaceholder.typicode.com/todos/1";
-            
+
             //var urlComplete = QueryHelpers.AddQueryString(urlBase, "SAMLRequest", "sdfadfd");
             var urlComplete = urlBase;
 
-            client.BaseAddress = new Uri(urlComplete);
-            client.DefaultRequestHeaders.Accept.Clear();
             //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"));
 
-            HttpResponseMessage response = await client.GetAsync("/");
+            var response = client.GetAsync(urlComplete).Result;
 
             if (response.IsSuccessStatusCode)
             {
-                
+                var result = response.Content.ReadAsStringAsync().Result;
             }
+
         }
     }
 }
